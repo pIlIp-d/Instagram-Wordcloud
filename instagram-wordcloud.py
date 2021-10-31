62423c8086b57b25d22e5459caa2681ca1273e16
Philip Dell <62812264+pIlIp-d@users.noreply.github.com>
2021-10-31 19:07:16 +0100

    clean code
    clean code, functions, structure

--- instagram-wordcloud.py
+++ instagram-wordcloud.py
@@ -11,103 +11,128 @@
 # ==================================================================
 
 ### options
-save = True # change to false if you just want to display the wordcloud
-max_words = 600
+MAX_WORDS = 600
 compress = True
 
 #colors
-colorscheme = "Pastel2" #https://matplotlib.org/stable/gallery/color/colormap_reference.html
+BACKGROUND = "black"
+COLORSCHEME = "cividis" #https://matplotlib.org/stable/gallery/color/colormap_reference.html
 
+'''
+wordcloud uses a random factor for fitting in the words
+-> little changes in the positioning even when the words are the same
+
+use any combination of colors to create a unique style 
+Here are some examples:
+______________________
+
+Background - black
+
+	#settled colors -- viridis
+	#settled colos -- cividis
+
+	#blue style -- winter
+	#light orange style -- Wistia
+	#dark orange -- autumn
+______________________
+
+Background -- white
+
+	#modern -- inferno
+	#brown -- copper
+	#light blue, pink -- cool
+______________________
+
+Tip for trying different styles - turn off compression
+______________________
+
+'''
 
 #time
 from datetime import datetime
 dt = datetime.now()
 time = datetime.timestamp(dt)
 
-###init
+###imports
 import csv, sys, os
 from wordcloud import WordCloud
-import matplotlib.pyplot as plt
-from bs4 import BeautifulSoup
 from os.path import exists
-
+from bs4 import BeautifulSoup
 import crunch
 
-### reading and formating words
-print("Instagram Wordcloud")
-wordlist = ""
+def read_files():
+	files = []
+	if exists("ads_interests.html"): files.append("ads_interests.html")
+	else: print("[!] ads_interests.html not found.")
+	if exists("your_topics.html"): files.append("your_topics.html")
+	else: print("[!] your_topics.html not found.")
+	return files
 
-files = []
-if exists("ads_interests.html"):
-	files.append("ads_interests.html")
-else:
-	print("[!] ads_interests.html not found.")
-if exists("your_topics.html"):
-	files.append("your_topics.html")
-else:
-	print("[!] your_topics.html not found.")
-#formating
-for file in files:
-	print("reading "+file)
-	htmldoc = open(file).read()
-	soup = BeautifulSoup(htmldoc, "html.parser")
-	tag = soup.find_all(class_="_23bw")
-	if tag == []:
-		print("[!] found no categories in html, check if the categories are in html-class called '_23bw'")
-		quit()
-	for i in tag:
-		st = i.string
-		st = st.replace("  "," ")
-		st = st.replace("(","")
-		st = st.replace("\n","")
-		st = st.replace("[beta]","")
-		st = st.replace("(","")
-		st = st.replace(")","")
-		st = st.replace(";","")
-		st = st.replace(".","")
-		st = st.replace("&"," ")
-		st = st.replace("-"," ")
-		wordlist += st + ","
+def format(files):	
+	wordlist = ""
+	for file in files:
+		print("reading "+file)
+		htmldoc = open(file).read()
+		tag = BeautifulSoup(htmldoc, "html.parser").find_all(class_="_23bw")
+		if tag == []:
+			print("[!] found no categories in html, check if the categories are in html-class called '_23bw'")
+			quit()
+		for i in tag:
+			st = i.string
+			st = st.replace("  "," ")
+			st = st.replace("(","")
+			st = st.replace("\n","")
+			st = st.replace("[beta]","")
+			st = st.replace("(","")
+			st = st.replace(")","")
+			st = st.replace(";","")
+			st = st.replace(".","")
+			st = st.replace("&"," ")
+			st = st.replace("-"," ")
+			wordlist += st + ","
+	return wordlist
 
-if wordlist == "":
-	print("[!] No words found!")
-	quit()
-### Generate a word cloud image
-print("generating wordcloud\n. . .")
-wordcloud = WordCloud(font_path="fonts/PathwayExtreme.ttf", width=3200, height=1600, max_words=max_words, min_word_length=2, scale = 2,colormap=colorscheme).generate(wordlist)
-plt.figure( figsize=(20,10), facecolor='k')
-plt.imshow(wordcloud, interpolation="bilinear")
-plt.axis("off")
-
-### save or show wordcloud
-path_exists = False
-filenum = 1
-while not path_exists:
-	if exists("interests_wordcloud_"+str(filenum)+".png"):
-		filenum += 1
-	else:
-		path = "interests_wordcloud_"+str(filenum)+".png"
-		path_exists = True
-if (save):
+def create_wordcloud(wordlist, path):
+	print("generating wordcloud\n. . .")
+	wordcloud = WordCloud(
+		font_path="fonts/PathwayExtreme.ttf", 
+		width=3200, height=1600, max_words=MAX_WORDS, min_word_length=2, scale = 2,
+		colormap=COLORSCHEME, background_color=BACKGROUND
+	).generate(wordlist)
 	wordcloud.to_file(path)
-	#plt.savefig(path)
 	print("--wordcloud saved--")
-else:
-	print("--showing wordcloud--")
-	plt.show()
 
+def get_path():
+	### save or show wordcloud
+	filenum = 1
+	while True:
+		if not exists("interests_wordcloud_"+str(filenum)+".png"):
+			return "interests_wordcloud_"+str(filenum)+".png", filenum
+		else: filenum += 1
 
-#compression
-if compress:
-	print("--Compressing via Crunch--")	
+def compresser(path,filenum):
+	print("--Compressing via Crunch--")
+	compressed = "interests_wordcloud_"+str(filenum)+"-crunch.png"
+	path = "interests_wordcloud_"+str(filenum)+".png"
 	crunch.crunch(path)
 	#replace with compressed file
-	compressed = "interests_wordcloud_"+str(filenum)+"-crunch.png"
 	os.remove(path)
 	os.rename(compressed, path)
 
-##time
-dt = datetime.now()
-print("\nProcess finished in "+str(datetime.timestamp(dt) - time)[0:5] + " seconds.")
+def __main__():
+	print("Instagram Wordcloud")
+	files = read_files()
+	wordlist = format(files)
+	if wordlist == "":
+		print("[!] No words found!")
+		quit()
 
-print("\n--programm closed--")
+	path, filenum = get_path()
+	create_wordcloud(wordlist, path)
+	if compress:
+		compresser(filenum)
+
+	dt = datetime.now()
+	print("\nProcess finished in "+str(datetime.timestamp(dt) - time)[0:5] + " seconds.")
+	print("\n--programm closed--")
+__main__()
