53e2f41ca7bd68a612509155a524b8df5f93b752
Philip Dell <62812264+pIlIp-d@users.noreply.github.com>
2021-10-31 14:24:49 +0100

    changes v3
    support for 'your_topcis.html'
    compression via crunch
    process time included

--- instagram-wordcloud.py
+++ instagram-wordcloud.py
@@ -1,52 +1,98 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 #by Philip Dell, MIT-Licence, v2 19.10.2021
 
+#time
+from datetime import datetime
+dt = datetime.now()
+time = datetime.timestamp(dt)
+
 ###init
-import csv, sys
+import csv, sys, os
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 from bs4 import BeautifulSoup
+from os.path import exists
+
+import crunch
 
 save = True # change to false if you just want to display the wordcloud
 
 ### reading and formating words
-print("reading categories\n")
-htmldoc = open("ads_interests.html").read()
-soup = BeautifulSoup(htmldoc, "html.parser")
-tag = soup.find_all(class_="_23bw")
+print("Instagram Wordcloud")
 wordlist = ""
-if tag == []:
-	print("error: found no categories in html, check if the categories are in html-class called '_23bw'")
+
+files = []
+if exists("ads_interests.html"):
+	files.append("ads_interests.html")
+else:
+	print("[!] ads_interests.html not found.")
+if exists("your_topics.html"):
+	files.append("your_topics.html")
+else:
+	print("[!] your_topics.html not found.")
+
+for file in files:
+	print("reading "+file)
+	htmldoc = open(file).read()
+	soup = BeautifulSoup(htmldoc, "html.parser")
+	tag = soup.find_all(class_="_23bw")
+	if tag == []:
+		print("[!] found no categories in html, check if the categories are in html-class called '_23bw'")
+		quit()
+	for i in tag:
+		st = i.string
+		st = st.replace("  "," ")
+		st = st.replace("(","")
+		st = st.replace("\n","")
+		st = st.replace("[beta]","")
+		st = st.replace("(","")
+		st = st.replace(")","")
+		st = st.replace(";","")
+		st = st.replace(".","")
+		st = st.replace("&"," ")
+		st = st.replace("-"," ")
+		wordlist += st + ","
+
+if wordlist == "":
+	print("[!] No words found!")
 	quit()
-
-for i in tag:
-	st = i.string
-	st = st.replace("  "," ")
-	st = st.replace("(","")
-	st = st.replace("\n","")
-	st = st.replace("[beta]","")
-	st = st.replace("(","")
-	st = st.replace(")","")
-	st = st.replace(";","")
-	st = st.replace(".","")
-	st = st.replace("&"," ")
-	st = st.replace("-"," ")
-	wordlist += st + ","
-
 ### Generate a word cloud image
 print("generating wordcloud\n. . .")
-wordcloud = WordCloud().generate(wordlist)
-wordcloud = WordCloud(width=1600, height=800).generate(wordlist)
+wordcloud = WordCloud(font_path="PathwayExtreme.ttf", width=3200, height=1600, max_words=600, min_word_length=2, scale = 2).generate(wordlist)#colormap scale
 plt.figure( figsize=(20,10), facecolor='k')
 plt.imshow(wordcloud, interpolation="bilinear")
 plt.axis("off")
 
-
 ### save or show wordcloud
+path_exists = False
+filenum = 1
+while not path_exists:
+	if exists("interests_wordcloud_"+str(filenum)+".png"):
+		filenum += 1
+	else:
+		path = "interests_wordcloud_"+str(filenum)+".png"
+		path_exists = True
 if (save):
-	plt.savefig("interests_wordcloud.png")
+	wordcloud.to_file(path)
+	#plt.savefig(path)
 	print("--wordcloud saved--")
 else:
 	print("--showing wordcloud--")
 	plt.show()
-	
-print("--programm closed--")
+
+
+#compression
+print("--Compressing via Crunch--")	
+crunch.crunch(path)
+#replace with compressed file
+compressed = "interests_wordcloud_"+str(filenum)+"-crunch.png"
+os.remove(path)
+os.rename(compressed, path)
+
+##time
+dt = datetime.now()
+print("\nProcess finished in "+str(datetime.timestamp(dt) - time)[0:5] + "seconds.")
+
+print("\n--programm closed--")
